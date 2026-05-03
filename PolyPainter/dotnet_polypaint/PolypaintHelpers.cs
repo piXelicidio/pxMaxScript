36c8f4de08811b6e8dee72d89c3ca6db39240f3a
Denys Almaral <pxtracer@gmail.com>
2026-05-03 15:17:23 -0400

    adding .Net helpers to PolyPainter

---
+++ PolyPainter/dotnet_polypaint/PolypaintHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_polypaint
+{
+    public static class PolypaintHelpers
+    {
+        public static int sum(int a, int b)
+        {
+            return a * 2 + b * 2;
+        }
+    }
+}
---
+++ PolyPainter/dotnet_polypaint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_polypaint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PolypaintHelpers.sum(5, 5));
+        }
+    }
+}
