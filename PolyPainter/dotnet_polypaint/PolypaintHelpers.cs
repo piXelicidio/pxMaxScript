2675e7c22d63973e15eaba7d1e9b25257cac14a1
Denys Almaral <pxtracer@gmail.com>
2026-05-03 15:56:46 -0400

    compiling csharp test

--- PolyPainter/dotnet_polypaint/PolypaintHelpers.cs
+++ PolyPainter/dotnet_polypaint/PolypaintHelpers.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace dotnet_polypaint
 {
-    public static class PolypaintHelpers
+    public class PolypaintHelpers
     {
-        public static int sum(int a, int b)
+        public int sum(int a, int b)
         {
             return a * 2 + b * 2;
         }
